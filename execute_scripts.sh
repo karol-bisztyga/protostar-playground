f14ca102fbaabb4c04fccafc4655d33a27a335a0
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-12-06 12:54:17 +0100

    lil fixes in the script

--- execute_scripts.sh
+++ execute_scripts.sh
@@ -6,10 +6,14 @@ set -e
 
 echo "BUILD"
 ./protostar build
+echo "TEST"
+./protostar test
 echo "DECLARE"
 
 OUT=$(./protostar declare ./build/main.json --json)
 echo "DEPLOY"
 CLASS_HASH=$(/Users/karolbisztyga/.asdf/installs/python/3.9.14/bin/python -c "import sys, json; print(json.loads(sys.argv[1])['class_hash'])" $OUT)
 echo "class hash: $CLASS_HASH"
-./protostar deploy $CLASS_HASH
+./protostar deploy $CLASS_HASH --inputs 100
+
+echo "DONE"
